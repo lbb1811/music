15570030147aa1524f8296803f262d0de59a6663
lbb1811 <liubin_2014@outlook.com>
2019-03-19 12:39:16 +0800

    first

---
+++ src/api/config.js
@@ -0,0 +1,13 @@
+export const commonParams = {
+  g_tk: 5381,
+  inCharset: 'uft-8',
+  outCharset: 'utf-8',
+  notice: 0,
+  format: 'json' // jsonp
+}
+
+export const options = {
+  param: 'jsonpCallback'
+}
+
+export const ERR_OK = 0
---
+++ src/components/rank/rank.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    rank 待开发
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
